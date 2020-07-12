99fc42dabc842c17dfb5463f0df129f8e6202d92
Berke Soysal <Berke.Soysal@sahibinden.com>
2020-07-12 18:58:01 +0300

    usr

---
+++ src/test/java/com/example/leadershipranking/UserControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.leadershipranking;
+
+import com.example.leadershipranking.models.UserProfile;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class UserControllerTest extends AbstractTest
+{
+    @Test
+    public void createUser() throws Exception {
+        String uri = "https://limitless-thicket-20437.herokuapp.com/leadership";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("display_name", "user1");
+        jsonObject.put("country", "TR");
+
+        String inputJson = super.mapToJson(jsonObject);
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri)
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson)).andReturn();
+
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        UserProfile userProfile = super.mapFromJson(content, UserProfile.class);
+        assertNotNull(userProfile.getUuid());
+    }
+}
